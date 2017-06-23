55dd20b1107d926c7921ccc5e184d07312f9bda4
Muhammad Farid <mfaired@gmail.com>
2017-06-23 14:42:53 +0200

    Arbian Safty

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var lateastCorusesSlider = new Swiper('.lateastCorusesSlider .swiper-container', {
+
+});
+
+var albumsSlider = new Swiper('.albumsSlider .swiper-container', {
+	nextButton: '.albumsSlider-button-next',
+    prevButton: '.albumsSlider-button-prev'
+});
+
+new WOW().init();
